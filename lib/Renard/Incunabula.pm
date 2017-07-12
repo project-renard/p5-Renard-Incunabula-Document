3dff92c3697bba8a1d9c5756b26207cb8b654c64
Zakariyya Mughal <zaki.mughal@gmail.com>
2017-07-11 23:42:52 -0500

    Prep for release v0.003

---
+++ lib/Renard/Incunabula.pm
@@ -0,0 +1,18 @@
+use Renard::Incunabula::Common::Setup;
+package Renard::Incunabula;
+# ABSTRACT: The base library for Project Renard
+
+1;
+
+__END__
+
+=head1 DESCRIPTION
+
+This library contains various modules that can be used throughout other parts
+of Project Renard.  Among these is L<Renard::Incunabula::Common::Setup> which
+provides an easy way to export commonly used features such as use of
+L<Function::Parameters> for function signatures and type-checking.
+
+=head1 SEE ALSO
+
+L<Repository information|http://project-renard.github.io/doc/development/repo/p5-Renard-Incunabula/>
